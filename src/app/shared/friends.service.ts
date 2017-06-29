3806f4c2b64d419a931035c9c9efda311f2caa01
Raju Gandhi <raju.gandhi@gmail.com>
2017-06-28 20:19:27 -0400

    feat: introduce a saveFriend method to PUT a friend on to the backend

--- src/app/shared/friends.service.ts
+++ src/app/shared/friends.service.ts
@@ -16,10 +16,20 @@ const BASE_URL = 'http://localhost:3000';
   providedIn: 'root'
 })
 export class FriendsService {
+  private headers: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  });
 
   constructor(private http: HttpClient) { }
 
   getFriends(): Observable<Friend[]> {
     return this.http.get<Friend[]>(`${BASE_URL}/friends`);
+  }
+
+  saveFriend(friend: Friend): Observable<Friend> {
+    return this.http.put<Friend>(`${BASE_URL}/friends/${friend.id}`,
+      JSON.stringify(friend),
+      { headers: this.headers });
   }
 }
