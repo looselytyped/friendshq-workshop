7e758e5bc2122b2ddfb39487ca866e8288bee9cc
Raju Gandhi <raju.gandhi@gmail.com>
2017-06-30 17:29:34 -0400

    feat: use routes, and router-outlet to set up the router state correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {
@@ -13,6 +14,10 @@ import {
   FriendsService,
 } from "./shared";
 
+import {
+  routes,
+} from "./app.routes";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +28,7 @@ import {
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [
     FriendsService,
