54412af6e99d3015648368feda3a5a0cff0a370e
Raju Gandhi <raju.gandhi@gmail.com>
2017-07-01 07:23:09 -0400

    feat: flesh out the PeopleComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {
+  PeopleComponent,
+} from './people';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
   ],
   imports: [
     BrowserModule,
