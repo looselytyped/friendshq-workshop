16c30100f87a726900541c41623e09ca0df38fe4
Raju Gandhi <raju.gandhi@gmail.com>
2017-07-01 07:38:38 -0400

    feat: introduce the Friend domain, and use it in PersonListComponent

--- src/app/people/person-list/person-list.component.ts
+++ src/app/people/person-list/person-list.component.ts
@@ -1,11 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  Friend,
+} from "../../shared";
+
+const FRIENDS: Friend[] = [
+  {
+    id: 1,
+    first_name: 'Michelle',
+    last_name: 'Mulroy',
+    gender: 'female',
+    fav: true,
+  },
+  {
+    id: 2,
+    first_name: 'Venkat',
+    last_name: 'Subramanian',
+    gender: 'male',
+    fav: true,
+  },
+  {
+    id: 3,
+    first_name: 'Matt',
+    last_name: 'Forsythe',
+    gender: 'none',
+    fav: false,
+  },
+  {
+    id: 4,
+    first_name: 'Brian',
+    last_name: 'Sletten',
+    gender: 'male',
+    fav: false,
+  },
+];
+
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.component.html',
   styleUrls: ['./person-list.component.css']
 })
 export class PersonListComponent implements OnInit {
+  friends: Friend[] = FRIENDS;
 
   constructor() { }
 
---
+++ src/app/shared/friend.model.ts
@@ -0,0 +1,7 @@
+export class Friend {
+  id: number;
+  first_name: string;
+  last_name: string;
+  gender: string;
+  fav: boolean;
+}
