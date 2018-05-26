16c3ef17933b8312dd14b1caab218ca47a8576f0
Raju Gandhi <raju.gandhi@gmail.com>
2018-05-26 12:23:33 -0400

    feat: use the FriendService to retrieve all the friends

--- src/app/people/person-list/person-list.component.ts
+++ src/app/people/person-list/person-list.component.ts
@@ -2,38 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import {
   Friend,
+  FriendsService,
 } from "../../shared";
-
-const FRIENDS: Friend[] = [
-  {
-    id: 1,
-    firstName: 'Michelle',
-    lastName: 'Mulroy',
-    gender: 'female',
-    fav: true,
-  },
-  {
-    id: 2,
-    firstName: 'Venkat',
-    lastName: 'Subramanian',
-    gender: 'male',
-    fav: true,
-  },
-  {
-    id: 3,
-    firstName: 'Matt',
-    lastName: 'Forsythe',
-    gender: 'none',
-    fav: false,
-  },
-  {
-    id: 4,
-    firstName: 'Brian',
-    lastName: 'Sletten',
-    gender: 'male',
-    fav: false,
-  },
-];
 
 @Component({
   selector: 'app-person-list',
@@ -41,12 +11,14 @@ const FRIENDS: Friend[] = [
   styleUrls: ['./person-list.component.css']
 })
 export class PersonListComponent implements OnInit {
-  friends: Friend[] = FRIENDS;
+  friends: Friend[] = [];
   displayBanner: boolean = false;
 
-  constructor() { }
+  constructor(private friendsService: FriendsService) { }
 
   ngOnInit() {
+    this.friendsService.getFriends()
+      .subscribe(friends => this.friends = friends);
   }
 
   showBanner(friend: Friend) {
