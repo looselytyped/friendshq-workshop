e753f3a235b87c2eb88572133fdeb01c2d3fb9c2
Raju Gandhi <raju.gandhi@gmail.com>
2018-05-26 12:18:16 -0400

    feat: introduce ShowPersonComponent and wire it up with *ngFor to list friends

---
+++ src/app/people/show-person/show-person.component.ts
@@ -0,0 +1,21 @@
+import {
+  Component,
+  OnInit,
+  Input,
+} from '@angular/core';
+
+import { Friend } from "../../shared";
+
+@Component({
+  selector: 'app-show-person',
+  templateUrl: './show-person.component.html',
+  styleUrls: ['./show-person.component.css']
+})
+export class ShowPersonComponent implements OnInit {
+  @Input() friend: Friend;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
